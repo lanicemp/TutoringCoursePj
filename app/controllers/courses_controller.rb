ba14472e2d7960deab8068fb91605676b9c31f48
LaNice Powell <lanicemp@aol.com>
2020-01-23 15:53:54 -0500

    updated with methods

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,13 @@
 class CoursesController < ApplicationController
+
+    before_action :authenticate_user!
+    
+    def show 
+        @course = Course.find(params[:id])
+    end 
+
+    def index
+        @courses = Course.all
+    end 
+    
 end
