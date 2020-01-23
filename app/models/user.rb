dd8eeeca847b4fdc9bf37a65650d8ac2f6325d7c
LaNice Powell <lanicemp@aol.com>
2020-01-23 15:56:07 -0500

    updated realtionships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+has_many :assignments
+has_many :courses, through: :assignments
+validates :email, presence: true 
+validates :password, presence:true 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
