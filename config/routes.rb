597787382d3e73608b3906ccd99ec940ff973c99
LaNice Powell <lanicemp@aol.com>
2020-01-23 16:08:11 -0500

    created views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :courses
+
   resources :assignments
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'application#welcome'
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'}
