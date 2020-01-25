ae76ecdf7c1f9e134728f7f53ec1d7172cdef8bb
LaNice Powell <lanicemp@aol.com>
2020-01-24 22:22:59 -0500

    updated show method

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,32 @@
 class AssignmentsController < ApplicationController
+    
+    before_action :authenticate_user!
+    
+    def show
+        @assignment = Assignment.find_by(id: params[:id])
+    end
+    
+    def index
+    @assignments = Assignment.all
+    end
+
+    def new
+    @assignment = Assignment.new
+    # @assignment.courses.build
+
+    end
+
+    def create
+    assignment = Assignment.create(assignment_params)
+    redirect_to assignment
+    end
+
+    private
+
+    def post_params
+    params.require(:assignment).permit(:title, :content, course_ids:[], :course_attributes => [:name])
+    end
 end
+  
+    
+
