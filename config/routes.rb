b49e005235cd621f0b8a35c3a5e8a335d1f18fce
LaNice Powell <lanicemp@aol.com>
2020-01-24 22:40:37 -0500

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :submissions
+  resources :enrollments
   resources :courses
-
   resources :assignments
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'application#welcome'
