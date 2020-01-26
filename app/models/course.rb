d67572a6c66ec5487a26c073b772978541dba816
LaNice Powell <lanicemp@aol.com>
2020-01-26 07:58:31 -0500

    updated syntanx of the relationships

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,11 @@ class Course < ApplicationRecord
     has_many :enrollments
     has_many :students, through: :enrollments, source: :user
 
-    
+    # accepts_nested_attributes_for :teacher reject_if: :all_blank
 
+    # validates :content, presence: true
+    # validates :content, length: { minimum: 10 }
+    validates :course_name, :presence => true
+    validates :description, :presence => true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
 has_many :enrollments
 has_many :courses, through: :enrollments
+has_many :courses_as_teacher, class_name: "Course", foreign_key: :teacher_id
 has_many :assignments, through: :courses
 has_many :submissions
 has_many :submitted_assignments, through: :submissions, source: :assignment
