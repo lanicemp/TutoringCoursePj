bc56451426c8f4c61e90469c00eda3d96e2ae5ab
LaNice Powell <lanicemp@aol.com>
2020-01-28 12:40:39 -0500

    updated new and create method

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -12,19 +12,31 @@ class AssignmentsController < ApplicationController
 
     def new
         @assignment = Assignment.new
+        @assignment = current_user.assignments_as_teacher.new(course_id: params[:course_id])
     # @assignment.courses.build
 
     end
 
     def create
-        assignment = Assignment.create(assignment_params)
-        redirect_to assignment
+binding.pry
+        @assignment = current_user.assignments_as_teacher.create(assignment_params)
+    
+        if @assignment.save
+            
+            redirect_to new_assignment_path
+        else 
+            binding.pry
+            redirect_to  course_path 
+        end 
+
+
     end
 
     private
 
     def assignment_params
-    params.require(:assignment).permit(:title, :content, course_ids:[], :course_attributes => [:name])
+    params.require(:assignment).permit(:assignment_name, :description, :q_1, :q_2, :q_3, :course_id)
+    # :course_attributes => [:name]
     end
 end
   
