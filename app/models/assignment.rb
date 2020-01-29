ab566b6e0224541540fdcaa1e936a21a46dc62ef
LaNice Powell <lanicemp@aol.com>
2020-01-29 08:05:16 -0500

    updated validations

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,5 +3,7 @@ class Assignment < ApplicationRecord
     belongs_to :course 
     has_many :submissions
     # has_many :assignments_as_teacher
-    #accepts_nested_attributes_for :course, reject_if: :all_blank
+    accepts_nested_attributes_for :course, reject_if: :all_blank
+
+    validates :assignment_name, :presence => true, uniqueness: true 
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,14 +3,18 @@ class Course < ApplicationRecord
     has_many :assignments
     has_many :enrollments
     has_many :students, through: :enrollments, source: :user
-    # has_many :assignments_as_teacher
+    has_many :assignments_as_teacher do 
+        def assignments_as_teacher
+            includes(:assignments, :teacher_id)
+        end 
+    end 
 
     # accepts_nested_attributes_for :assignments, reject_if: :all_blank
     # accepts_nested_attributes_for :teacher reject_if: :all_blank
 
     # validates :content, presence: true
     # validates :content, length: { minimum: 10 }
-    validates :course_name, :presence => true
+    validates :course_name, :presence => true, uniqueness: true 
     validates :description, :presence => true
 
 end
