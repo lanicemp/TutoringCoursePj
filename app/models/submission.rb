8696c22db23a4df1cd30eead7f4172e9fe0f718a
LaNice Powell <lanicemp@aol.com>
2020-01-31 09:44:34 -0500

    updated relationships adding foreign Key and scope

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,13 @@
 class Submission < ApplicationRecord
 
-    belongs_to :student, class_name: 'User'
+    belongs_to :student, class_name: 'User', foreign_key: :user_id
     belongs_to :assignment
 
+    scope :by_user, ->(user) { where( user_id: user.id)}
+
+
 end
+#  submission = assignment.submissions.by_user(current_user).first
+# if submission
+# show a link_to the submission with text of complete
+# else if there's no submission then show a link to the new assignemnt submission path
