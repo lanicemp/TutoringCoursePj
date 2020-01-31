c28aafe68459ede0501bbe169ab3f0fd8506d67b
LaNice Powell <lanicemp@aol.com>
2020-01-31 09:40:34 -0500

    connected the User to the Submission and added flash messages

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -13,18 +13,25 @@ class SubmissionsController < ApplicationController
 
     def new
         @submission = Submission.new
+      
+        @assignment  = Assignment.find(params[:assignment_id])
+
+        @submission = @assignment.submissions.build
+        @submission.user_id = current_user.id
+        # @assignment = current_user.new(course_id: params[:course_id])
        
     end
 
     def create
         
-        @submission = Submission.create(submission_params)
-    
+        @submission = current_user.submissions.build(submission_params)
+        @submission.user_id = current_user.id
         if @submission.save
-            
+            flash[:success] = "Your Assignment has been Submitted!!"
             redirect_to new_course_assignment_path(@submission)
         else 
-            
+            binding.pry 
+            flash[:error] = "Your Assignment has Not Submitted."
             redirect_to  new_course_path  
         end 
 
@@ -40,7 +47,7 @@ class SubmissionsController < ApplicationController
 
     private
 
-    def course_params
-        params.require(:course_id, :assignment_id).permit(:a_1, :a_2, :a_3)
+    def submission_params
+        params.require(:submission).permit(:a_1, :a_2, :a_3, :assignment_id, :user_id)
     end
 end
