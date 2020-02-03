b94f1842d3cb9ec1fbaadab37ad504ebb523a964
LaNice Powell <lanicemp@aol.com>
2020-02-03 14:23:34 -0500

    added notes

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,6 +1,9 @@
 class EnrollmentsController < ApplicationController
     before_action :authenticate_user!
 
-    
+    #add a create and a new with a form pass the current insert a then redirect to the courses assignments
+    #link to the 
+    #validattion on the assignmets to put a vaildation that the student is enrolled
+    #
 
 end
