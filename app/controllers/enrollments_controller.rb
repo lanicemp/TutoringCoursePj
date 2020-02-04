13ec00c6100e2699ae61b61bc64bd5c00e87ed36
LaNice Powell <lanicemp@aol.com>
2020-02-04 16:17:53 -0500

    updated new, create added private fixed params, and before action

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,14 +1,16 @@
 class EnrollmentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_course
 
     def new
-        @enrollment = Enrollment.new
+        @enrollment = @course.enrollments.build
        
     end
 
     def create
-        @enrollment = current_user.enrollment.build(enrollment_params)
-        @enrollment.user_id = current_user.id
+        binding.pry 
+        @enrollment = current_user.enrollments.build(enrollment_params)
+        @enrollment.student_id = current_user.id
 
         if @enrollment.save
             flash[:success] = "Your Enrolled!!"
@@ -17,25 +19,29 @@ class EnrollmentsController < ApplicationController
             flash[:error] = "Error Try again!!"
             redirect_to  new_course_path  
         end 
+    end 
 
-        def index
-         
-            if params[:course_id]
-                 @course = Course.find(params[:course_id]) 
-                 @enrollments = @course.enrollments
-                 @teacher = @course.teacher == current_user
-            else 
-                 @assignments = Assignment.all
-            end 
-     
-        end
+    def index
+        
+        if params[:course_id]
+                @course = Course.find(params[:course_id]) 
+                @enrollments = @course.enrollments
+                @teacher = @course.teacher == current_user
+        else 
+                @assignments = Assignment.all
+        end 
+    
+    end
 
+    private 
 
-        def enrollment_params
-            params.require(:submission).permit(:course_id, :user_id)
-        end
-    #add a create and a new with a form pass the current insert a then redirect to the courses assignments
-    #link to the 
+    def enrollment_params
+        params.require(:enrollment).permit(:course_id, :student_id)
+    end
+    
+    def set_course
+        @course = Course.find(params[:course_id])
+    end 
     #validattion on the assignmets to put a vaildation that the student is enrolled
     #
 
