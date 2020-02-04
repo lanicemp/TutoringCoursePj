9b0658216015be7e4aece5d14104009a200f38fe
LaNice Powell <lanicemp@aol.com>
2020-02-04 10:14:20 -0500

    created enrollments controller and added new, create, and index methods.

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,6 +1,39 @@
 class EnrollmentsController < ApplicationController
     before_action :authenticate_user!
 
+    def new
+        @enrollment = Enrollment.new
+       
+    end
+
+    def create
+        @enrollment = current_user.enrollment.build(enrollment_params)
+        @enrollment.user_id = current_user.id
+
+        if @enrollment.save
+            flash[:success] = "Your Enrolled!!"
+            redirect_to new_course_assignment_path(@enrollment)
+        else 
+            flash[:error] = "Error Try again!!"
+            redirect_to  new_course_path  
+        end 
+
+        def index
+         
+            if params[:course_id]
+                 @course = Course.find(params[:course_id]) 
+                 @enrollments = @course.enrollments
+                 @teacher = @course.teacher == current_user
+            else 
+                 @assignments = Assignment.all
+            end 
+     
+        end
+
+
+        def enrollment_params
+            params.require(:submission).permit(:course_id, :user_id)
+        end
     #add a create and a new with a form pass the current insert a then redirect to the courses assignments
     #link to the 
     #validattion on the assignmets to put a vaildation that the student is enrolled
