e6f69f30e9af4eba97ed4c238696c3eab17408d7
LaNice Powell <lanicemp@aol.com>
2020-02-05 07:15:16 -0500

    nested submissions under assignment relationship

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resources :assignments
     resources :enrollments
   end 
-  resources :assignments, except: :index 
+  resources :assignments, except: :index do 
+    resources :submissions
+  end 
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -14,5 +16,6 @@ Rails.application.routes.draw do
   devise_scope :user do 
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
+    
   end 
 end
