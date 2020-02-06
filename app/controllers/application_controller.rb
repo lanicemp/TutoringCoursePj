88ba9bc3ccf8030feab4dabe8378409e5b4891bb
LaNice Powell <lanicemp@aol.com>
2020-02-06 01:34:54 -0500

    updated welcome path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
 
     def welcome 
+        if  !user_signed_in? 
+            redirect_to login_path
+        end 
     end 
     
     helpers do 
